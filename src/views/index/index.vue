ca7a625f6d165f1958d71bc4819bd5b99f3dd92f
ma <mazhaoyou_gis@163.com>
2019-10-18 16:51:24 +0800

    vue.config.js 文件配置

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,19 +1,79 @@
 <<template>
-<el-button type="primary" style="width:100%;" @click="handleLogin" :loading="logining">登录</el-button>
+<div >
+<div>
+<!-- <el-button type="primary" style="width:100%;" @click="handleLogin" :loading="logining">登录</el-button>
+ --><el-row class="tac">
+  <el-col :span="12">
+    <!-- <h5>默认颜色</h5> -->
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+    </el-menu>
+  </el-col>
+</el-row>
+</div>
+<!-- <div class="url-sty">
+  <p>{{url}}</p>
+</div> -->
+</div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      url: process.env.VUE_APP_URL
+    };
   },
   methods: {
     handleLogin() {
       this.$router.push({ path: "/login" });
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
 };
 </script>
-<style lang="sass" scoped>
 
+<style lang="scss" scoped>
+.url-sty{
+  width: 500px;
+  height: 200px;
+  background: red;
+}
+.tac{
+  width:20%;
+}
 </style>
 
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,39 @@
+module.exports = {
+    baseUrl: '/', // 根路径
+    outputDir: 'dist', // 构建输出目录
+    assetsDir: 'assets', // 静态资源目录(js,css,img,fonts)
+    lintOnSave: false, // 是否开启eslint保存检测, 有效值: true || false || 'error'
+    devServer: {
+        open: true, //自动启动浏览器
+        host: 'localhost',
+        port: 8081,
+        https: false, //启动https 协议
+        hotOnly: false,
+        proxy: {
+            // 配置跨域
+            '/api': {
+                target: 'http//localhost:5000/api/',
+                ws: true,
+                changOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        },
+        before(app) {
+            // http://localhost:8081/api/goods
+            app.get('/api/goods', (req, res) => {
+                res.json(goods);
+            });
 
+            app.get('/api/ratings', (req, res) => {
+                res.json(ratings);
+            });
+
+            app.get('/api/seller', (req, res) => {
+                res.json(seller);
+            });
+        }
+    }
+
+}
